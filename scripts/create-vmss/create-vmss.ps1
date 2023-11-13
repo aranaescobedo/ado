4cf716bf3b141e49ebb41c960aeebdf07ebe8518
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-11-13 23:13:25 +0100

    Update create-vmss.ps1

--- scripts/create-vmss/create-vmss.ps1
+++ scripts/create-vmss/create-vmss.ps1
@@ -35,6 +35,7 @@ param(
 $getRootDir = git rev-parse --show-toplevel
 Set-Location "$getRootDir\<FILE_PATH>" #The placeholder <FILE_PATH> represents the file path or directory where the script is located within your Git repository.
 
+$appregistrationName = ""
 $extensionName = "AADSSHLoginForLinux"
 $resouceGroupName = ""
 $routeTableName = ""
